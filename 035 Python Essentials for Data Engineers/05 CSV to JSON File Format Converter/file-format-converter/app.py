96680f6c29b0c6bc2df0669641754a50cfb6be0a
Ken Wood <ken@roarmarketingconcepts.com>
2025-01-11 16:45:44 -0500

    modified:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/app.py 	deleted:    035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/categories/part-00000 	deleted:    035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/customers/part-00000 	deleted:    035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/departments/part-00000 	deleted:    035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/products/part-00000 	modified:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/hw.py

--- 035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/app.py
+++ 035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/app.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import json
+import sys
 import re
 import pandas as pd
 
@@ -33,22 +34,36 @@ def to_json(df, tgt_base_dir, ds_name, file_name):
     
 def file_converter(src_base_dir, tgt_base_dir, ds_name):
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
-    files = glob.glob(f'{src_base_dir}/{ds_name}/part-*')
-
+    files = glob.glob(f'{src_base_dir}/{ds_name}/*')
+    if len(files) == 0:
+        raise NameError(f'No files found for table "{ds_name}"')   # raise an error if no files found because we can't process empty files
     for file in files:
         df = read_csv(file, schemas)
         file_name = re.split('[/\\\\]', file)[-1]
         to_json(df, tgt_base_dir, ds_name, file_name)
         
 def process_files(ds_names=None):
-    src_base_dir = 'data/retail_db'
-    tgt_base_dir = 'data/retail_db_json'
+    # src_base_dir = 'data/retail_db'
+    # tgt_base_dir = 'data/retail_db_json'
+    src_base_dir = os.environ.get('SRC_BASE_DIR')
+    tgt_base_dir = os.environ.get('TGT_BASE_DIR')
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     if not ds_names:
         ds_names = schemas.keys()
     for ds_name in ds_names:
-        print(f'Processing {ds_name}')
-        file_converter(src_base_dir, tgt_base_dir, ds_name)
+    # add try-except block to catch any errors that occur during processing
+        try:        
+            print(f'Processing {ds_name}')
+            file_converter(src_base_dir, tgt_base_dir, ds_name)
+        except NameError as ne:
+            print(f'Error processing {ds_name}: {ne}')   # print error message if NameError occurs  
+            pass
         
 if __name__ == '__main__':
-    process_files()
+    
+    if len(sys.argv) == 2:
+        tables = json.loads(sys.argv[1])  # provide list of tables to process   
+    else:
+        tables = None         # process all tables in schemas.json
+        
+    process_files(tables)
--- 035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/hw.py
+++ 035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/hw.py
@@ -1,12 +1,17 @@
 import sys
 import os
+import json
 
 args = sys.argv
 arg = args[1]
 prog_file = args[0]
+print(f'Arguments: {args}')
+print(f'Program file: {prog_file}')
+print(f'Argument: {arg}')
 
-print(f'Hello {arg} from {prog_file}')  
-os.environ 
+print(json.loads(arg))
+
+
 
 # appdev -> appdbdev(retail_db,retail_user, retaildevpassword)
 # appuat -> appdbuat(retail_db,retail_user, retailuatpassword)$
