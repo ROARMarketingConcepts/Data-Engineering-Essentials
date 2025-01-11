2d8bd597df458568a99ee85629f983212e2f4516
Ken Wood <ken@roarmarketingconcepts.com>
2025-01-11 11:18:33 -0500

    modified:   .DS_Store 	modified:   035 Python Essentials for Data Engineers/.DS_Store 	modified:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/.DS_Store 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/.DS_Store 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/app.py 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/categories/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/create_db_tables_pg.sql 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/customers/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/departments/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/load_db_tables_pg.sql 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/order_items/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/orders/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/products/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db/schemas.json 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/categories/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/customers/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/departments/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/order_items/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/orders/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/data/retail_db_json/products/part-00000 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/hw.py 	new file:   035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/requirements.txt

---
+++ 035 Python Essentials for Data Engineers/05 CSV to JSON File Format Converter/file-format-converter/hw.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+args = sys.argv
+arg = args[1]
+prog_file = args[0]
+
+print(f'Hello {arg} from {prog_file}')  
+os.environ 
+
+# appdev -> appdbdev(retail_db,retail_user, retaildevpassword)
+# appuat -> appdbuat(retail_db,retail_user, retailuatpassword)$
